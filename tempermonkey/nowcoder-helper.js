ed3dde61ae5661e33437e6697aa0d93e32049ad5
Richard <18270414620@139.com>
2021-12-28 15:39:12 +0800

    记忆上次设置的隐藏模式

--- tempermonkey/nowcoder-helper.js
+++ tempermonkey/nowcoder-helper.js
@@ -7,11 +7,31 @@
 // @match        https://www.nowcoder.com/test/question/*
 // @match        https://www.nowcoder.com/question/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    /**
+     * 隐藏head 和 foot
+     */
+    function hidef(){
+        let header = document.getElementsByClassName("header-main");
+        let subact = document.getElementsByClassName("subject-action");
+        let footer = document.getElementsByClassName("js-nowcoder-footer"); // com-subject-wrap
+        header[0].style.display = "none";
+        subact[0].style.display = "none";
+        footer[0].style.display = "none";
+    }
+
+    // 获取全局变量
+    let enableXMode = GM_getValue("x", false)
+    if(enableXMode){
+        hidef();
+    }
 
     // Your code here...
     // 监听键盘事件
@@ -25,12 +45,14 @@
             return;
         }
         if(key == 'N'){
-            let answing = document.getElementsByClassName("answering-num");
-           
+            let answingnum = parseInt( document.getElementsByClassName("answering-num")[0].innerText)
+            let sheet = document.getElementsByClassName("answer-sheet-num")
+            sheet[0].children[answingnum-2].children[0].click()
             return;
         }
         // 显示 网页头， 摸鱼模式
         if(key === 'x'){
+            GM_setValue("x", true)
             let header = document.getElementsByClassName("header-main");
             let subact = document.getElementsByClassName("subject-action");
             let footer = document.getElementsByClassName("js-nowcoder-footer"); // com-subject-wrap
@@ -39,6 +61,7 @@
             footer[0].style.display = "none";
         }
         if(key === 'X'){
+            GM_setValue("x", false)
             let header = document.getElementsByClassName("header-main");
             let subact = document.getElementsByClassName("subject-action");
             let footer = document.getElementsByClassName("js-nowcoder-footer");
